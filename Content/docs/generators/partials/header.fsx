0579718db15da84f7c1abfb38f1ce12535fca84f
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-06-18 19:49:19 +0200

    Simplify css files

--- Content/docs/generators/partials/header.fsx
+++ Content/docs/generators/partials/header.fsx
@@ -16,11 +16,10 @@ let header (ctx : SiteContents) page =
         meta [Name "viewport"; Content "width=device-width, initial-scale=1"]
         title [] [!! (siteInfo.title + " | " + page)]
         link [Rel "icon"; Type "image/png"; Sizes "32x32"; Href (rootUrl.subRoute "/static/images/favicon.png")]
-        link [Rel "stylesheet"; Href (rootUrl.subRoute "/static/css/nucleus.css")]
-        link [Rel "stylesheet"; Href (rootUrl.subRoute "/static/css/hybrid.css")]
         link [Rel "stylesheet"; Href (rootUrl.subRoute "/static/css/atom-one-dark-reasonable.css")]
         link [Rel "stylesheet"; Href (rootUrl.subRoute "/static/css/theme.css")]
         link [Rel "stylesheet"; Href (rootUrl.subRoute "/static/css/tips.css")]
+
         link [Rel "stylesheet"; Href "//cdnjs.cloudflare.com/ajax/libs/JavaScript-autoComplete/1.0.4/auto-complete.min.css"]
         link [Rel "stylesheet"; Href "//cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css"]
         link [Rel "stylesheet"; Href "//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css"]
